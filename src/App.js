ef490795e79701aa80f51e0fd93cf8654b13e3da
Liang <392425595@qq.com>
2020-04-13 16:26:31 +0800

    添加Antd v4 样式组件库支持

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Button type="primary" href="#">
           Learn React
-        </a>
+        </Button>
       </header>
     </div>
   );
